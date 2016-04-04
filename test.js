3612f8ab378cff40c1aded1a805552a48c7e900f
jan vonrickenbach <jan.vonrickenbach@greenmatch.ch>
2016-04-04 21:50:59 +0200

    Bispiil

---
+++ test.js
@@ -0,0 +1,29 @@
+$(document).ready(machDasAllesWennReady);
+
+function machDasAllesWennReady(){
+
+
+}
+
+function machAndersch(){
+    $("p").html("andersch");
+    $("#Sarah").html("Sarah bliibt glich");
+    $("#Mario").addClass("important");
+}
+
+
+
+function machMuure(stei){
+    console.log("Ich bu en muure mit ",stei)
+}
+
+
+
+var hauptStei = "gneis";
+var chliniStei = "granit";
+
+machMuure(hauptStei);
+machMuure(chliniStei);
+machMuure(chliniStei);
+machMuure(chliniStei);
+machMuure(hauptStei);
